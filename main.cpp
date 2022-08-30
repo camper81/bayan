d0d34bfd315f8150eed907637d991f6b301af5b0
sxd_maxim <mbelickyan@gmail.com>
2022-08-30 15:36:42 +0300

    Clean code. Added md5 hash

--- main.cpp
+++ main.cpp
@@ -1,79 +1,49 @@
+#include <iostream>
+#include <vector>
 #include <string>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/uuid/name_generator_sha1.hpp>
 #include <boost/uuid/name_generator_md5.hpp>
-#include <boost/crc.hpp>
-#include <iostream>
-#include <vector>
-#include <list>
-#include <array>
-#include <iomanip>
-#include <stack>
+#include <boost/regex.hpp>
 
-namespace po = boost::program_options;
-
-class Duplicator {
-
-};
-
-struct hash{
-    hash(const char* seq);
-    virtual void calc(const char* seq, size_t size) = 0;
-    virtual bool operator==(hash& other) = 0;
-};
-
-struct hash_sha1 : public hash{
-   void calc(const char* seq, size_t size) {
-       boost::uuids::detail::sha1 h;
-       h.process_bytes(seq, size);
-   }
-   bool operator==(hash& other) {
-       other.operator==(*this);
-   }
-
-    bool operator==(hash_sha1& other) {
-       size_t size = sizeof(calculated_sum) / sizeof(calculated_sum[0]);
-       for(size_t i = 0; i < size; ++i)
-           if(other.calculated_sum[i] != calculated_sum[i])
-               return false;
-
-       return true;
+struct hash_md5_digits{
+    unsigned int digits[4];
+    hash_md5_digits(const char* str, size_t block_size) {
+        boost::uuids::detail::md5 sha;
+        sha.process_bytes(str, block_size);
+        sha.get_digest(digits);
     }
 
-    int calculated_sum[5] {0};
+    bool operator<(const hash_md5_digits& other) const {
+        return std::tie(this->digits[0],this->digits[1],this->digits[2],this->digits[3]) <
+               std::tie(other.digits[0],other.digits[1],other.digits[2],other.digits[3]);
+    }
+    bool operator==(const hash_md5_digits& other) const {
+        return std::tie(this->digits[0],this->digits[1],this->digits[2],this->digits[3]) ==
+               std::tie(other.digits[0],other.digits[1],other.digits[2],other.digits[3]);
+    }
 };
 
-struct file_info {
-    file_info(const std::string& p) : filepath(p) {}
-    std::string filepath;
-    std::vector<std::shared_ptr<hash>> hash;
-};
-
-struct comparator{
-    comparator(std::string path) : fs_(path) {};
-    std::fstream fs_;
-    bool isOpen = true;
-};
-
-struct hash_digits{
+struct hash_sha_digits{
     unsigned int digits[5];
-    hash_digits(const char* str, size_t block_size) {
+    hash_sha_digits(const char* str, size_t block_size) {
         boost::uuids::detail::sha1 sha;
         sha.process_bytes(str, block_size);
         sha.get_digest(digits);
     }
 
-    bool operator<(const hash_digits& other) const {
+    bool operator<(const hash_sha_digits& other) const {
         return std::tie(this->digits[0],this->digits[1],this->digits[2],this->digits[3],this->digits[4]) <
                 std::tie(other.digits[0],other.digits[1],other.digits[2],other.digits[3],other.digits[4]);
     }
-    bool operator==(const hash_digits& other) const {
+    bool operator==(const hash_sha_digits& other) const {
         return std::tie(this->digits[0],this->digits[1],this->digits[2],this->digits[3],this->digits[4]) ==
                std::tie(other.digits[0],other.digits[1],other.digits[2],other.digits[3],other.digits[4]);
     }
 };
 
+template<typename HASH_ALGORITHM>
 void compareFiles(std::vector<std::string>& filenames, size_t block_size, size_t chunk_cnt){
     struct file {
         std::fstream* fs;
@@ -103,12 +73,12 @@ void compareFiles(std::vector<std::string>& filenames, size_t block_size, size_t
 
     boost::uuids::detail::sha1 h;
     while(--chunk_cnt) {
-        std::map<hash_digits, identity> hash;
+        std::map<HASH_ALGORITHM, identity> hash;
         for(auto& sub_vector: stack) {
             for(auto& elem: sub_vector) {
                 elem.first->read(&buf[0], block_size);
                 h.process_bytes(&buf[0], block_size);
-                hash_digits dig(&buf[0], block_size);
+                HASH_ALGORITHM dig(&buf[0], block_size);
                 hash[dig].same_files++;
                 hash[dig].open_files.push_back(elem);
             }
@@ -124,11 +94,11 @@ void compareFiles(std::vector<std::string>& filenames, size_t block_size, size_t
 
     }
 
-    for(auto& filevec : stack) {
+    for(auto& filevec: stack) {
+        std::cout << std::endl;
         for(auto& file : filevec) {
             std::cout << file.second << std::endl;
         }
-        std::cout << std::endl;
     }
 
     for(auto& file : opened_files) {
@@ -137,6 +107,8 @@ void compareFiles(std::vector<std::string>& filenames, size_t block_size, size_t
 }
 
 int main(int argc, char** argv){
+    namespace po = boost::program_options;
+
     std::map<size_t, std::vector<std::string>> cntBlocksToFilename;
 
     po::options_description desc;
@@ -149,20 +121,10 @@ int main(int argc, char** argv){
             ("level,l", po::value< int >(), "scanning level")
             ("hash,h", po::value<std::string>()->default_value("crc32"), "set hash function")
             ("size,s", po::value<int>()->default_value(4), "set block size")
+            ("pattern,p", po::value<std::string>(), "pattern for scanning file example \"text(\\\\d+).txt\"")
             ;
     po::variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
-
-    notify(vm);
-
-    if(vm.count("include")){
-        for(auto& s : vm["include"].as<std::vector<std::string>>())
-            std::cout << s << std::endl;
-    }
-    if(vm.count("exclude")) {
-        for (auto &s: vm["exclude"].as<std::vector<std::string>>())
-            std::cout << s << std::endl;
-    }
 
     const int block_size = vm["size"].as<int>();
     std::vector<char> buf(block_size, 0);
@@ -176,32 +138,44 @@ int main(int argc, char** argv){
     if(check_directories.empty())
             check_directories.emplace_back(boost::filesystem::current_path());
 
-    for(auto& dir : check_directories) {
-        for(auto& r_it: dir){
-            if(vm.count("level") && (dir.level() > vm["level"].as<int>()))
-                continue;
-            std::cout << dir.level() << std::endl;
-//            std::cout << r_it.path().branch_path().string() << std::endl;
-            if(vm.count("exclude"))
-                if(std::find(vm["exclude"].as<std::vector<std::string>>().begin(),
-                             vm["exclude"].as<std::vector<std::string>>().end(),
-                             r_it.path().branch_path()) != vm["exclude"].as<std::vector<std::string>>().end())
+        for(auto& dir : check_directories) {
+            for(auto& r_it: dir){
+                if(vm.count("level") && (dir.level() > vm["level"].as<int>()))
                     continue;
+                if(vm.count("exclude"))
+                    if(std::find(vm["exclude"].as<std::vector<std::string>>().begin(),
+                                 vm["exclude"].as<std::vector<std::string>>().end(),
+                                 r_it.path().branch_path()) != vm["exclude"].as<std::vector<std::string>>().end())
+                        continue;
 
-            std::cout << r_it.path().branch_path() << std::endl;
+               if(vm.count("pattern")) {
+                   try {
 
-            if(boost::filesystem::is_regular(r_it.path())) {
-                cntBlocksToFilename[boost::filesystem::file_size(r_it.path())/ block_size].emplace_back(r_it.path().string());
+                       boost::regex pattern(vm["pattern"].as<std::string>());
+                       if (!boost::regex_match(r_it.path().filename().string(), pattern)) {
 
+                           continue;
+                       }
+
+                    } catch(std::exception& ex) {
+                       std::cout << ex.what() << std::endl;
+                    }
+               }
+
+               if(boost::filesystem::is_regular(r_it.path())) {
+                   cntBlocksToFilename[boost::filesystem::file_size(r_it.path())/ block_size].emplace_back(r_it.path().string());
+               }
             }
         }
-    }
+
 
     for(auto& filevec : cntBlocksToFilename) {
         // with the same numbers of blocks
         if(filevec.second.size() > 1) {
-            compareFiles(filevec.second, block_size, filevec.first);
-            std::cout << std::endl;
+            if(vm.count("hash") && vm["hash"].as<std::string>() == "md5")
+                compareFiles<hash_md5_digits>(filevec.second, block_size, filevec.first);
+            else
+                compareFiles<hash_sha_digits>(filevec.second, block_size, filevec.first);
         }
     }
 
